4911d17a19883fc2c797c38a65a393f6698ea579
Maria Jose Tamayo <ma.jose.tamayo89@gmail.com>
2020-03-12 15:29:19 -0300

    estructura lista, falta reemplazar datos

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+﻿<!--<template>
+    <div id="app">
+        {{ message }}
+    </div>
+</template>
+<script>
+        export default {
+            data() {
+                return {
+                    message: 'Hola Desafío Latam!!! 🤟🏻🤟🏻',
+                };
+            }
+    };
+</script>
+<style>
+    #app {
+        font-size: 25px;
+        font-family: 'Calibri', sans-serif;
+        color: darkblue;
+        background: lightblue;
+    }
+</style>-->
